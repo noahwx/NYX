b7ad1f16a73307912b239f3b8bcbd04741d34d6b
noahwx <noahfontenette@outlook.com>
2023-03-09 15:13:56 -0600

    0.0.350

--- my-app/src/components/products.js
+++ my-app/src/components/products.js
@@ -4,74 +4,112 @@ import { NavLink } from 'react-router-dom';
 const products = () => {
     return ( 
         <div className='products'>
-            <div className='row1'>
-                <NavLink to='/' className='product'>
-                    <div className='productinfo'>
-                        <h2 className='producttitle'>Unknown Product</h2>
-                        <h4 className='productprice'>$0.00</h4>
-                    </div>
-                    <img className='productimg' src='https://images.unsplash.com/photo-1604072374690-0e7d7bddd54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt=''/>
-                </NavLink>
-                <NavLink to='/' className='product'>
-                    <div className='productinfo'>
-                        <h2 className='producttitle'>Unknown Product</h2>
-                        <h4 className='productprice'>$0.00</h4>
-                    </div>
-                    <img className='productimg' src='https://images.unsplash.com/photo-1604072374690-0e7d7bddd54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt=''/>
-                </NavLink>
-                <NavLink to='/' className='product'>
-                    <div className='productinfo'>
-                        <h2 className='producttitle'>Unknown Product</h2>
-                        <h4 className='productprice'>$0.00</h4>
-                    </div>
-                    <img className='productimg' src='https://images.unsplash.com/photo-1604072374690-0e7d7bddd54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt=''/>
-                </NavLink>
-            </div>
-            <div className='row2'>
-                <NavLink to='/' className='product'>
-                    <div className='productinfo'>
-                        <h2 className='producttitle'>Unknown Product</h2>
-                        <h4 className='productprice'>$0.00</h4>
-                    </div>
-                    <img className='productimg' src='https://images.unsplash.com/photo-1604072374690-0e7d7bddd54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt=''/>
-                </NavLink>
-                <NavLink to='/' className='product'>
-                    <div className='productinfo'>
-                        <h2 className='producttitle'>Unknown Product</h2>
-                        <h4 className='productprice'>$0.00</h4>
-                    </div>
-                    <img className='productimg' src='https://images.unsplash.com/photo-1604072374690-0e7d7bddd54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt=''/>
-                </NavLink>
-                <NavLink to='/' className='product'>
-                    <div className='productinfo'>
-                        <h2 className='producttitle'>Unknown Product</h2>
-                        <h4 className='productprice'>$0.00</h4>
-                    </div>
-                    <img className='productimg' src='https://images.unsplash.com/photo-1604072374690-0e7d7bddd54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt=''/>
-                </NavLink>
-            </div>
-            <div className='row3'>
-                <NavLink to='/' className='product'>
-                    <div className='productinfo'>
-                        <h2 className='producttitle'>Unknown Product</h2>
-                        <h4 className='productprice'>$0.00</h4>
-                    </div>
-                    <img className='productimg' src='https://images.unsplash.com/photo-1604072374690-0e7d7bddd54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt=''/>
-                </NavLink>
-                <NavLink to='/' className='product'>
-                    <div className='productinfo'>
-                        <h2 className='producttitle'>Unknown Product</h2>
-                        <h4 className='productprice'>$0.00</h4>
-                    </div>
-                    <img className='productimg' src='https://images.unsplash.com/photo-1604072374690-0e7d7bddd54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt=''/>
-                </NavLink>
-                <NavLink to='/' className='product'>
-                    <div className='productinfo'>
-                        <h2 className='producttitle'>Unknown Product</h2>
-                        <h4 className='productprice'>$0.00</h4>
-                    </div>
-                    <img className='productimg' src='https://images.unsplash.com/photo-1604072374690-0e7d7bddd54e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt=''/>
-                </NavLink>
+            <div className='rows'>
+                    <NavLink className='product row1'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row1'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row1'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row1'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row1'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row2'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row2'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row2'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row2'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row2'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row3'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row3'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row3'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row3'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
+                    <NavLink className='product row3'>
+                        <div className='productinfo'>
+                            <h1 className='productname'>Hoodie</h1>
+                            <p className='productprice'>$0.00</p>
+                        </div>
+                        <img className='productimg' src='https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' alt=''/>
+                    </NavLink>
             </div>
         </div>
      );
